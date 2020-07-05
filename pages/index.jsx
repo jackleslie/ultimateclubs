95f17467521c1afe1c78f9d98799dfa2d3adaff5
Jack Leslie <jjdl@st-andrews.ac.uk>
2020-07-05 01:04:49 +0100

    feat: can filter by name, removed button padding

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,7 +19,6 @@ import {
   Input,
   MenuGroup,
   Radio,
-  RadioGroup,
 } from '@chakra-ui/core';
 import { FaSortAlphaDown, FaSortAlphaUp, FaFilter } from 'react-icons/fa';
 
@@ -28,6 +27,10 @@ export default function Index({ teams }) {
   const [range, setRange] = useState([0, rangeIncrement - 1]);
   const [ascending, setAscending] = useState(true);
   const [filter, setFilter] = useState();
+  const [search, setSearch] = useState('');
+
+  const handleFilterChange = (e) => setFilter(e.target.value);
+  const handleSearchChange = (e) => setSearch(e.target.value);
 
   const next = () => setRange([range[0] + rangeIncrement, range[1] + rangeIncrement]);
   const prev = () => setRange([range[0] - rangeIncrement, range[1] - rangeIncrement]);
@@ -38,8 +41,15 @@ export default function Index({ teams }) {
   };
 
   const typeFilter = (({ type }) => (filter ? type === filter : true));
+  const searchFilter = (({ name }) => name.toLowerCase().indexOf(search.toLowerCase()) >= 0);
 
-  const teamsFilteredLength = teams.filter(typeFilter).length;
+  const teamsFilteredLength = teams.filter(typeFilter).filter(searchFilter).length;
+
+  const showMessage = () => (teamsFilteredLength > 0 ? (
+    `${range[0] + 1}-${range[1] >= teamsFilteredLength ? teamsFilteredLength : range[1] + 1} of ${teamsFilteredLength}`
+  ) : (
+    'No teams'
+  ));
 
   return (
     <Flex justify="center" width="100%">
@@ -48,30 +58,47 @@ export default function Index({ teams }) {
           <Heading textAlign="center">Club Ultimate UK</Heading>
           <Heading textAlign="center" size="md" fontWeight={600}>Team Directory</Heading>
         </Stack>
-        <Stack isInline spacing={4} width="100%">
+        <Stack isInline spacing={[2, 3, 4]} width="100%">
           <Button
             onClick={() => setAscending(!ascending)}
             width="10%"
+            p={0}
           >
             <Box as={ascending ? FaSortAlphaDown : FaSortAlphaUp} />
           </Button>
-          <Input aria-label="Team name input" placeholder="Team name" />
+          <Input aria-label="Team name input" placeholder="Team name" value={search} onChange={handleSearchChange} />
           <Menu>
-            <MenuButton as={Button} width="10%">
+            <MenuButton as={Button} width="10%" p={0}>
               <Box as={FaFilter} />
             </MenuButton>
             <MenuList>
               <MenuGroup title="Type">
-                <RadioGroup
+                <Stack
                   spacing={0}
-                  onChange={(e) => setFilter(e.target.value)}
-                  value={filter}
                   ml={4}
                 >
-                  <Radio value="School">School</Radio>
-                  <Radio value="Club">Club</Radio>
-                  <Radio value="University">University</Radio>
-                </RadioGroup>
+                  <Radio
+                    value="School"
+                    isChecked={filter === 'School'}
+                    onChange={handleFilterChange}
+                  >
+                    School
+                  </Radio>
+                  <Radio
+                    value="Club"
+                    isChecked={filter === 'Club'}
+                    onChange={handleFilterChange}
+                  >
+                    Club
+                  </Radio>
+                  <Radio
+                    value="University"
+                    isChecked={filter === 'University'}
+                    onChange={handleFilterChange}
+                  >
+                    University
+                  </Radio>
+                </Stack>
                 <Button size="md" ml={4} mt={3} mb={2} variant="link" fontWeight={400} onClick={() => setFilter()}>
                   Clear
                 </Button>
@@ -80,50 +107,45 @@ export default function Index({ teams }) {
           </Menu>
         </Stack>
         <Accordion allowMultiple width="100%">
-          {teams.sort(alphabeticalSort).filter(typeFilter).slice(...range).map(({
-            name, type, url, location,
-          }, index) => (
-            <AccordionItem key={`${name}-${index}`}>
-              <AccordionHeader>
-                <Flex align="center" justify="space-between" width="100%">
-                  <Stack align="baseline" spacing={0}>
-                    <Heading textAlign="left" size="sm">{name}</Heading>
-                    <Text>{type}</Text>
+          {teams.sort(alphabeticalSort).filter(typeFilter).filter(searchFilter).slice(...range)
+            .map(({
+              name, type, url, location,
+            }, index) => (
+              <AccordionItem key={`${name}-${index}`}>
+                <AccordionHeader>
+                  <Flex align="center" justify="space-between" width="100%">
+                    <Stack align="baseline" spacing={0}>
+                      <Heading textAlign="left" size="sm">{name}</Heading>
+                      <Text>{type}</Text>
+                    </Stack>
+                    <AccordionIcon />
+                  </Flex>
+                </AccordionHeader>
+                <AccordionPanel pb={4}>
+                  <Stack spacing={2}>
+                    <Box>
+                      <Heading size="xs">UK Ultimate team URL</Heading>
+                      <Link href={url} isExternal>
+                        {url}
+                        {' '}
+                        {url !== 'No URL available' ? <Icon name="external-link" mx="2px" /> : null}
+                      </Link>
+                    </Box>
+                    <Box>
+                      <Heading size="xs">Location</Heading>
+                      <Text>{location}</Text>
+                    </Box>
                   </Stack>
-                  <AccordionIcon />
-                </Flex>
-              </AccordionHeader>
-              <AccordionPanel pb={4}>
-                <Stack spacing={2}>
-                  <Box>
-                    <Heading size="xs">UK Ultimate team URL</Heading>
-                    <Link href={url} isExternal>
-                      {url}
-                      {' '}
-                      {url !== 'No URL available' ? <Icon name="external-link" mx="2px" /> : null}
-                    </Link>
-                  </Box>
-                  <Box>
-                    <Heading size="xs">Location</Heading>
-                    <Text>{location}</Text>
-                  </Box>
-                </Stack>
-              </AccordionPanel>
-            </AccordionItem>
-          ))}
+                </AccordionPanel>
+              </AccordionItem>
+            ))}
         </Accordion>
         <Stack isInline width="100%" justify="space-between" align="center" spacing={5}>
           <Button rightIcon="arrow-back" variantColor="gray" variant="outline" isDisabled={range[0] === 0} onClick={prev}>
             Prev
           </Button>
           <Text textAlign="center" fontSize={['sm', 'md']}>
-            {range[0] + 1}
-            -
-            {range[1] >= teamsFilteredLength ? teamsFilteredLength : range[1] + 1}
-            {' '}
-            of
-            {' '}
-            {teamsFilteredLength}
+            {showMessage()}
           </Text>
           <Button rightIcon="arrow-forward" variantColor="gray" variant="outline" isDisabled={range[1] >= teamsFilteredLength} onClick={next}>
             Next
